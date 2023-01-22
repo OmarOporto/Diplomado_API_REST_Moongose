52da3b59a3618c61b8f38b253fb4f7f1be31e990
OmarOporto <omar.oportober@gmail.com>
2023-01-22 11:59:27 -0400

    retoques finales

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const morgan = require("morgan");
 const mongoose= require('mongoose');
 require("dotenv").config();
 const routes_u = require("./routes/usuario");
 const routes_p = require("./routes/producto");
 
 const app= express();
+app.use(morgan('dev'));
 const port = process.env.PORT || 9000;
 
 //midlewares
@@ -19,6 +21,19 @@ app.get('/', (req,res) =>
     res.send("Welcome to my API");
 })
 
+//error condition D
+
+app.all("*", (req, res, next) => {
+    throw new Error('route not found');
+});
+
+app.use((err, req, res, next) => {
+    res.status(400).json({
+        status: "error",
+        message: err.message,
+    });
+});
+
 //mongodb connection
 
 mongoose
@@ -27,3 +42,5 @@ mongoose
     .catch((error)=>console.error(error))
 
 app.listen(port, () => console.log('server listen on port', port));
+
+module.exports = app;
